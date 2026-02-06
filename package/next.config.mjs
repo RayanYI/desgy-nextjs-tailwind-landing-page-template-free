787aa2d055b103aacd0b3fdff4cace1ea3ecb163
RayanYI <rayanyyousfi@gmail.com>
2026-02-06 04:26:11 +0100

    Fix jsdom dependencies

--- package/next.config.mjs
+++ package/next.config.mjs
@@ -3,15 +3,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // On garde les configs pour ignorer les erreurs de build
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // On dit à Next.js de ne pas essayer de bundler ces librairies capricieuses
-  serverExternalPackages: ["jsdom"],
+  // 👇 C'est ici qu'on force Vercel à laisser ces librairies tranquilles
+  serverExternalPackages: ["jsdom", "html-encoding-sniffer", "@exodus/bytes"],
 }
 
 export default nextConfig;
